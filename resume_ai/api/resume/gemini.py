e1d598a018a81cc615cecdbe049f5971e52c5419
ajayshivhare <ajayshivhare047@gmail.com>
2026-04-13 13:52:36 +0530

    removed static api

--- resume_ai/api/resume/gemini.py
+++ resume_ai/api/resume/gemini.py
@@ -3,8 +3,5 @@ import frappe
 
 def get_gemini():
     api_key = frappe.conf.get("GEMINI_API_KEY")
-    # api_key = "AIzaSyCEnbZxf1mKDwTXc0QepTwotLzD8SMH8as"
     genai.configure(api_key=api_key)
-    # return genai.GenerativeModel("gemini-2.5-flash")
     return genai.GenerativeModel("gemini-2.5-pro")
-    # return genai.GenerativeModel("gemini-3-pro-preview")
